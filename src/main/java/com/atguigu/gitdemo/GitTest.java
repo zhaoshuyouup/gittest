14ac789f57c08925b2fd749b9f56f0eed425c397
zhaoshuyouup <121145932+zhaoshuyouup@users.noreply.github.com>
2023-03-28 10:22:22 +0800

    Create GitTest.java

--- src/main/java/com/atguigu/gitdemo/GitTest.java
+++ src/main/java/com/atguigu/gitdemo/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("push test2");
         System.out.println("push test3");
+        System.out.println("push test4");
 
     }
 }
