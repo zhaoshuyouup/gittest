65e694f63a9aee54bc62c80263a5d1161620fc4c
zhaoshuyou <zhaoshuyou_up@163.com>
2023-03-28 10:16:48 +0800

    push test commit3

--- src/main/java/com/atguigu/gitdemo/GitTest.java
+++ src/main/java/com/atguigu/gitdemo/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("push test3");
 
     }
 }
